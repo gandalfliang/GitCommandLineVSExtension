ffdebfb72c2a9ca5ebeb1684b56c021fc6c8a574
梁泰琪 <liangtaiqi@cvte.com>
2018-02-02 19:07:37 +0800

    fix error

--- GitConsoleExtension/ToolWindow/GitHostWindow.cs
+++ GitConsoleExtension/ToolWindow/GitHostWindow.cs
@@ -28,7 +28,7 @@ namespace GitConsoleExtension
         /// </summary>
         public GitHostWindow() : base(null)
         {
-            this.Caption = "Git Intergrated Window";
+            this.Caption = "Git Integrated Window";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
